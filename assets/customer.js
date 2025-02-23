6291fc09597499f4e53ae424d46169da3383d638
superverygoodman <timeshort@naver.com>
2025-02-23 22:25:39 +0900

    Update customer.js

--- assets/customer.js
+++ assets/customer.js
@@ -1,1 +1,6 @@
 console.log("작동중?")
+window.addEventListener('DOMContentLoaded', () => {
+    fetch('https://worldtimeapi.org/api/timezone/Asia/Seoul')
+    .then(response => response.json())
+.then(resp => {console.log(resp)})
+});
