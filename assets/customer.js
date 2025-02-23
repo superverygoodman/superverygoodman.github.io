13548831767da3ff3fb06da10fdbe52d0ca4e221
superverygoodman <timeshort@naver.com>
2025-02-23 23:51:23 +0900

    Update customer.js

--- assets/customer.js
+++ assets/customer.js
@@ -1,23 +1,13 @@
-console.log("작동중?")
-    let cust_now;
-    let vari;
 window.addEventListener('DOMContentLoaded', async () => {
-    cust_now = await loadTime();
+    const data_load = await fetch('https://timeapi.io/api/Time/current/zone?timeZone=Asia/Seoul');
+    const data = await data_load.json();
+    let cust_now = new Date(data.dateTime);
+    console.log(cust_now.getTime())
     document.querySelectorAll(".reading-time").forEach(ele => {
-        let post = new Date(ele.dataset.posttime);
-        let time_diffrence = cust_now.getTime() -post.getTime();
+        let post = ele.dataset.posttime;
+        let time_diffrence = cust_now.getTime() -post;
+        console.log("post : "+post);
         console.log(time_diffrence)
         
     })
 });
-async function loadTime () {
-  const response = await fetch('https://timeapi.io/api/Time/current/zone?timeZone=Asia/Seoul');
-  const data = await response.json();
-  return new Date(data.datetime);
-}
-
-console.log("3차수정이요")
-console.log("3차수정이요")
-console.log("3차수정이요")
-console.log("3차수정이요")
-console.log("3차수정이요")
