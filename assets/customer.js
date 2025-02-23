b2004da012708f8ebb5c15613d8ba20559b6ff4e
superverygoodman <timeshort@naver.com>
2025-02-23 22:54:06 +0900

    Update customer.js

--- assets/customer.js
+++ assets/customer.js
@@ -1,10 +1,18 @@
 console.log("작동중?")
     let cust_now;
 window.addEventListener('DOMContentLoaded', () => {
-    fetch('https://worldtimeapi.org/api/timezone/Asia/Seoul')
-    .then(response => response.json())
-    .then(response => {
-        cust_now = new Date(response.datetime)
-        console.log(cust_now)
+    cust_now = loadTime();
+    document.querySelectorAll(".reading-time").forEach(ele => {
+        let post = new Date(ele.dataset.posttime);
+        let time_diffrence = post.getTime() - cust_now.getTime()
+        console.log(time_diffrence)
     })
 });
+async function loadTime () {
+    await fetch('https://worldtimeapi.org/api/timezone/Asia/Seoul')
+    .then(response => response.json())
+    .then(response => {
+        let time = new Date(response.datetime);
+        return time;
+    })
+}
