68011915c2ac97342f07ddc29f4945b5acc8b742
superverygoodman <timeshort@naver.com>
2025-02-24 00:13:55 +0900

    Update customer.js

--- assets/customer.js
+++ assets/customer.js
@@ -5,9 +5,23 @@ const korNow = new Date(utc+koreaTimeDiff);
 window.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll(".reading-time").forEach(ele => {
         let post = ele.dataset.posttime;
-        let time_diffrence = korNow.getTime() -post;
+        let time_difference = korNow.getTime() -post;
         console.log("post : "+post);
-        console.log(time_diffrence)
-        
+        console.log(time_difference)
+        if (time_difference < 60 ) {
+            ele.innerText = (parseInt(time_difference)) + "초 전"
+        } else if (time_difference < 3600) {
+            ele.innerText = (parseInt(time_difference/60)) + "분 전"
+        } else if (time_difference < 86400) {
+            ele.innerText = (parseInt(time_difference/3600)) + "시간 전"
+        } else if (time_difference < 2592000 ) {
+            ele.innerText = (parseInt(time_difference/86400)) + " 일 전"
+        } else if (time_difference < 31536000 ) {
+            ele.innerText = (parseInt(time_difference/2592000)) + "개월월 전"
+        } else if (time_difference > 31536000) {
+            ele.innerText = (parseInt(time_difference/31536000)) + "년 전"
+        } else {
+
+        }
     })
 });
